de83d1f31e61223319a7698b3d06aa65187ed8e4
Vishal <127185682+Vishy70@users.noreply.github.com>
2025-04-09 16:27:15 +0530

    Filter added, no error, but does not filter correctly

--- qdisc.sh
+++ qdisc.sh
@@ -25,8 +25,8 @@ else
 fi
 
 # Read tx rings present on rs_veth... change it to 2
-ip netns exec router ethtool -L r_veth tx 2
-ip netns exec router ethtool -l r_veth
+sudo ip netns exec router ethtool -L r_veth tx 2
+sudo ip netns exec router ethtool -l r_veth
 
 # Add the mq classful qdisc, and add two separate queues
 # TODO: make this cli-arg!!!
@@ -37,11 +37,32 @@ sudo ip netns exec router tc qdisc add dev r_veth root handle 1: mq
 sudo ip netns exec router tc qdisc add dev r_veth parent 1:1 handle 10: fq_pie
 # Attach fq_codel to the second class
 sudo ip netns exec router tc qdisc add dev r_veth parent 1:2 handle 20: fq_codel
+# Add clsact: surprise???
+sudo ip netns exec router tc qdisc add dev r_veth clsact
 
 # Verify changes
 sudo ip netns exec router tc qdisc show
 
+
+
 # Add filter!
-sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 1 u32 match ip dst 192.168.1.2/24 action skbedit queue_mapping 0
-sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 2 matchall action skbedit queue_mapping 1
-sudo ip netns exec router tc filter show
+# sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 1 u32 match ip dst 192.168.1.2/24 flowid 1:1
+# sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 2 matchall flowid 1:2
+# sudo ip netns exec router tc filter show
+sudo ip netns exec router tc filter add dev r_veth egress protocol ip prio 1 u32 \
+    match ip dst 192.168.1.2/24 \
+    action skbedit queue_mapping 0
+
+sudo ip netns exec router tc filter add dev r_veth egress protocol ip prio 2 u32 \
+    match u32 0 0 \
+    action skbedit queue_mapping 1
+
+echo -e "\n"
+
+# Show filters
+sudo ip netns exec router tc -s filter show dev r_veth egress
+
+echo -e "\n"
+
+# Show qdisc stats
+sudo ip netns exec router tc -s qdisc show dev r_veth

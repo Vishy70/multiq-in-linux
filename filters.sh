e9a5d2577f043b7980556491401e1e22f80dc62f
Vishy70 <vishalkamath.221cs261@nitk.edu.in>
2025-10-22 20:10:21 +0530

    Filter moved to ifb device

--- filters.sh
+++ filters.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-SERVER1_IP="192.168.0.2"
+SERVER1_IP="192.168.2.2"
 
-echo -e "\e[34m Filters  \e[0m"
+echo -e "\e[34m Filters \e[0m"
 
 #ref for filters: https://www.kernel.org/doc/html/v5.8/networking/multiqueue.html
 
-sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 1 u32 match ip dst $SERVER1_IP action skbedit queue_mapping 0
-sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 2 matchall action skbedit queue_mapping 1
+sudo ip netns exec router tc filter add dev ifbcs parent 1: protocol ip prio 1 u32 match ip src $SERVER1_IP action skbedit queue_mapping 0
+sudo ip netns exec router tc filter add dev ifbcs parent 1: protocol ip prio 2 matchall action skbedit queue_mapping 1
+
+sudo ip netns exec router tc filter add dev ifbsc parent 1: protocol ip prio 1 u32 match ip src $SERVER1_IP action skbedit queue_mapping 0
+sudo ip netns exec router tc filter add dev ifbsc parent 1: protocol ip prio 2 matchall action skbedit queue_mapping 1
 
 # sudo ip netns exec router tc filter add dev r_veth parent 1: protocol ip prio 2 matchall action skbedit queue_mapping 1
 
 echo
 echo "Router's Filter Configuration "
-sudo ip netns exec router tc -s filter show dev r_veth parent 1:
+sudo ip netns exec router tc -s filter show dev ifbcs parent 1:
+sudo ip netns exec router tc -s filter show dev ifbsc parent 1:
 
 echo "-----------------------------------------------------------------------------------------------------------------------"
 echo

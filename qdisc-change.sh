a9f27720732769f945f6063a46abea6bcf498722
Vishy70 <vishalkamath.221cs261@nitk.edu.in>
2025-10-22 20:09:58 +0530

    Utilize ifb for link control, ensure proper qdisc add and removal

--- qdisc-change.sh
+++ qdisc-change.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
-NEW_QDISC=$1
+baseline=$1
+QDISC_1=$2
+QDISC_2=$3
 
-# Attach pfifo to the first class ALWAYS
-sudo ip netns exec router tc qdisc add dev r_veth parent 1:1 handle 10: pfifo
-# Attach NEW_QDISC to the second class
-sudo ip netns exec router tc qdisc add dev r_veth parent 1:2 handle 20: $NEW_QDISC
+if [ $baseline = "true" ];
+then
+    #ifbcs_root=$(sudo ip netns exec router tc qdisc show dev ifbcs handle 1: 2>/dev/null | awk '/[[:space:]]root[[:space:]]/ {print $2; exit}')
+    #ifbsc_root=$(sudo ip netns exec router tc qdisc show dev ifbsc handle 1: 2>/dev/null | awk '/[[:space:]]root[[:space:]]/ {print $2; exit}')
+
+    sudo ip netns exec router tc qdisc del dev ifbcs root handle 1:
+    sudo ip netns exec router tc qdisc del dev ifbsc root handle 1:
+    
+    sudo ip netns exec router tc qdisc add dev ifbcs root handle 1: $QDISC_1
+    sudo ip netns exec router tc qdisc add dev ifbsc root handle 1: $QDISC_1
+else
+    # Attach QDISC_1 to the first class
+    # Attach QDISC_2 to the second class
+    #ifbcs_child1=$(sudo ip netns exec router tc qdisc show dev ifbcs handle 10: 2>/dev/null | awk '/[[:space:]]parent[[:space:]]/ {print $2; exit}')
+    #ifbcs_child2=$(sudo ip netns exec router tc qdisc show dev ifbcs handle 20: 2>/dev/null | awk '/[[:space:]]parent[[:space:]]/ {print $2; exit}')
+
+    sudo ip netns exec router tc qdisc del dev ifbcs parent 1:1 handle 10:
+    sudo ip netns exec router tc qdisc del dev ifbcs parent 1:2 handle 20:
+    sudo ip netns exec router tc qdisc add dev ifbcs parent 1:1 handle 10: $QDISC_1
+    sudo ip netns exec router tc qdisc add dev ifbcs parent 1:2 handle 20: $QDISC_2
+
+    # Attach QDISC_1 to the first class
+    # Attach QDISC_2 to the second class
+    #ifbsc_child1=$(sudo ip netns exec router tc qdisc show dev ifbsc handle 10: 2>/dev/null | awk '/[[:space:]]parent[[:space:]]/ {print $2; exit}')
+    #ifbsc_child2=$(sudo ip netns exec router tc qdisc show dev ifbsc handle 20: 2>/dev/null | awk '/[[:space:]]parent[[:space:]]/ {print $2; exit}')
+
+    sudo ip netns exec router tc qdisc del dev ifbsc parent 1:1 handle 10:
+    sudo ip netns exec router tc qdisc del dev ifbsc parent 1:2 handle 20:
+    sudo ip netns exec router tc qdisc add dev ifbsc parent 1:1 handle 10: $QDISC_1
+    sudo ip netns exec router tc qdisc add dev ifbsc parent 1:2 handle 20: $QDISC_2
+fi
 
 if [ $? -eq 2 ]
 then
-
-    echo "Specified qdisc: !$NEW_QDISC!"
+    echo "Possible error?!"
+    echo "Specified qdiscs: $QDISC_1, $QDISC_2"
     exit 2
 fi
 

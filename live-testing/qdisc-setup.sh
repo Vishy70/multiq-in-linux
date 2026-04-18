24961ca6acc5ae98861143b7592b73a036a63a42
Vishy70 <vishalkamath.221cs261@nitk.edu.in>
2026-04-18 21:35:51 +0530

    Cleanup qdisc scripts

--- live-testing/qdisc-setup.sh
+++ live-testing/qdisc-setup.sh
@@ -6,20 +6,16 @@ baseline="$1"
 dev_name="$2"
 serial_num="$3"
 
-
+# TODO: make setup handle real and virtual topologies
 if [ $baseline = "false" ];
 then
     # adding multiq
-    #ifbcs_root=$(sudo ip netns exec router tc qdisc show dev ifbcs handle 1: 2>/dev/null | awk '/[[:space:]]root[[:space:]]/ {print $2; exit}')
-    #ifbsc_root=$(sudo ip netns exec router tc qdisc show dev ifbsc handle 1: 2>/dev/null | awk '/[[:space:]]root[[:space:]]/ {print $2; exit}')
-    
-    #sudo ip netns exec router tc qdisc del dev ifbcs root handle 1:
-    #sudo ip netns exec router tc qdisc del dev ifbsc root handle 1:
     ./adb -s $serial_num shell tc qdisc del dev $dev_name root handle 1:
     ./adb -s $serial_num shell tc qdisc add dev $dev_name root handle 1: multiq
     ./adb -s $serial_num shell tc qdisc add dev $dev_name parent 1:1 handle 10: pfifo
     ./adb -s $serial_num shell tc qdisc add dev $dev_name parent 1:2 handle 20: pfifo
 else
+    ./adb -s $serial_num shell tc qdisc del dev $dev_name clsact
     ./adb -s $serial_num shell tc qdisc add dev $dev_name root handle 1: pfifo
 fi
 

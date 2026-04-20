c99043911cc213626f497bdc9dc28dacfda6d9b1
Vishy70 <vishalkamath.221cs261@nitk.edu.in>
2026-04-20 13:07:04 +0530

    Remove live-testing

--- live-testing/udp_rtt_server.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(("192.168.1.2", 12345))
-
-while True:
-    data, addr = server_socket.recvfrom(2048)
-    server_socket.sendto(data, addr)
